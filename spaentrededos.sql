eec7adcde45f26cdda758f715f02086700be1d0f
TG <78963868+MatiasTG7@users.noreply.github.com>
2025-10-18 16:29:24 -0300

    Add files via upload

---
+++ spaentrededos.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-10-2025 a las 21:25:43
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spaentrededos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `codCli` int(11) NOT NULL,
+  `dni` varchar(15) NOT NULL,
+  `nombreCliente` varchar(100) NOT NULL,
+  `apellidoCliente` varchar(100) NOT NULL,
+  `telefonoCliente` varchar(15) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `afecciones` varchar(100) NOT NULL,
+  `estadoCliente` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dia_de_spa`
+--
+
+CREATE TABLE `dia_de_spa` (
+  `codPack` int(11) NOT NULL,
+  `FechayHora` datetime NOT NULL,
+  `preferencias` varchar(200) NOT NULL,
+  `codCli` int(11) NOT NULL,
+  `monto` double NOT NULL,
+  `estadoDia` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instalacion`
+--
+
+CREATE TABLE `instalacion` (
+  `codInstal` int(11) NOT NULL,
+  `nombreInstalacion` varchar(20) NOT NULL,
+  `detalleDeUso` varchar(100) NOT NULL,
+  `precio30m` double NOT NULL,
+  `estadoInstalacion` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `masajista`
+--
+
+CREATE TABLE `masajista` (
+  `codMasajista` int(11) NOT NULL,
+  `matricula` varchar(20) NOT NULL,
+  `nombreMasajista` varchar(20) NOT NULL,
+  `telefonoMasajista` varchar(15) NOT NULL,
+  `especialidad` enum('facial','corporal','relajacion','estetico') NOT NULL,
+  `estadoMasajista` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sesion`
+--
+
+CREATE TABLE `sesion` (
+  `codSesion` int(11) NOT NULL,
+  `fechaInicio` datetime NOT NULL,
+  `fechaFin` datetime NOT NULL,
+  `codTratamiento` int(11) NOT NULL,
+  `codMasajista` int(11) NOT NULL,
+  `codPack` int(11) NOT NULL,
+  `codInstal` int(11) NOT NULL,
+  `estadoInstalacion` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `codTratamiento` int(11) NOT NULL,
+  `nombreTratamiento` varchar(20) NOT NULL,
+  `tipo` enum('facial','corporal','relajacion','estetico') NOT NULL,
+  `detalleTratamiento` varchar(100) NOT NULL,
+  `duracionTratamiento` int(11) NOT NULL,
+  `costoTratamiento` double NOT NULL,
+  `activo` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`codCli`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  ADD PRIMARY KEY (`codPack`),
+  ADD UNIQUE KEY `idCliente` (`codCli`);
+
+--
+-- Indices de la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  ADD PRIMARY KEY (`codInstal`);
+
+--
+-- Indices de la tabla `masajista`
+--
+ALTER TABLE `masajista`
+  ADD PRIMARY KEY (`codMasajista`),
+  ADD UNIQUE KEY `matricula` (`matricula`);
+
+--
+-- Indices de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD PRIMARY KEY (`codSesion`),
+  ADD UNIQUE KEY `idDiaSpa` (`codPack`),
+  ADD UNIQUE KEY `idMasajista` (`codMasajista`),
+  ADD UNIQUE KEY `idTratamiento` (`codTratamiento`),
+  ADD UNIQUE KEY `codInstal` (`codInstal`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`codTratamiento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `codCli` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  MODIFY `codPack` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  MODIFY `codInstal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `masajista`
+--
+ALTER TABLE `masajista`
+  MODIFY `codMasajista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  MODIFY `codSesion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `codTratamiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dia_de_spa`
+--
+ALTER TABLE `dia_de_spa`
+  ADD CONSTRAINT `dia_de_spa_ibfk_1` FOREIGN KEY (`codCli`) REFERENCES `cliente` (`codCli`);
+
+--
+-- Filtros para la tabla `sesion`
+--
+ALTER TABLE `sesion`
+  ADD CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`codPack`) REFERENCES `dia_de_spa` (`codPack`),
+  ADD CONSTRAINT `sesion_ibfk_2` FOREIGN KEY (`codTratamiento`) REFERENCES `tratamiento` (`codTratamiento`),
+  ADD CONSTRAINT `sesion_ibfk_3` FOREIGN KEY (`codMasajista`) REFERENCES `masajista` (`codMasajista`),
+  ADD CONSTRAINT `sesion_ibfk_4` FOREIGN KEY (`codInstal`) REFERENCES `instalacion` (`codInstal`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
