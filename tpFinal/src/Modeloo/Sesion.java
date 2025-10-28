0d252bc8358c19265661909720a83f8c816fab4e
TG <matutemariano@gmail.com>
2025-10-27 23:35:55 -0300

    sesion y data

--- tpFinal/src/Modeloo/Sesion.java
+++ tpFinal/src/Modeloo/Sesion.java
@@ -1,5 +1,91 @@
 package Modeloo;
 
+import java.time.LocalDate;
+
 public class Sesion {
-    
+    private int codSesion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private int codTratamiento;
+    private int codMasajista;
+    private int codPack;
+    private boolean estadoInstalacion;
+
+    public Sesion() {
+    }
+
+    public Sesion(int codSesion, LocalDate fechaInicio, LocalDate fechaFin, int codTratamiento, int codMasajista, int codPack, boolean estadoInstalacion) {
+        this.codSesion = codSesion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.codTratamiento = codTratamiento;
+        this.codMasajista = codMasajista;
+        this.codPack = codPack;
+        this.estadoInstalacion = estadoInstalacion;
+    }
+
+    public int getCodSesion() {
+        return codSesion;
+    }
+
+    public void setCodSesion(int codSesion) {
+        this.codSesion = codSesion;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getCodTratamiento() {
+        return codTratamiento;
+    }
+
+    public void setCodTratamiento(int codTratamiento) {
+        this.codTratamiento = codTratamiento;
+    }
+
+    public int getCodMasajista() {
+        return codMasajista;
+    }
+
+    public void setCodMasajista(int codMasajista) {
+        this.codMasajista = codMasajista;
+    }
+
+    public int getCodPack() {
+        return codPack;
+    }
+
+    public void setCodPack(int codPack) {
+        this.codPack = codPack;
+    }
+
+    public boolean isEstadoInstalacion() {
+        return estadoInstalacion;
+    }
+
+    public void setEstadoInstalacion(boolean estadoInstalacion) {
+        this.estadoInstalacion = estadoInstalacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Sesion{" + "codSesion=" + codSesion + ", fechaInicio=" + fechaInicio + ", fechaFin" + fechaFin + ", codTratamiento" + codTratamiento + ", codMasajista=" + codMasajista + ", codPack=" + codPack + ", estadoInstalacion" + "}";
+        
+        
+        
+    }
+        
 }
--- tpFinal/src/Persistencia/SesionData.java
+++ tpFinal/src/Persistencia/SesionData.java
@@ -1,6 +1,68 @@
 
 package Persistencia;
 
+import Modeloo.Cliente;
+import Modeloo.Conexion;
+import Modeloo.Sesion;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Connection;
+
 public class SesionData {
     
+    private Connection con = null;
+    public SesionData(Connection con){
+    this.con = con;
+    }
+  public void insertarSesion(Sesion sesion) {
+        String sql = "INSERT INTO Sesion (codSesion, fechaInicio, fechaFin, codTratamiento, codMasajista, codPack,"
+                + " estadoInstalacion) " + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try{
+     PreparedStatement ps= con.prepareStatement(sql);
+        ps.setInt(1, sesion.getCodSesion());
+        ps.setDate(2, Date.valueOf(sesion.getFechaInicio()));
+        ps.setDate(3, Date.valueOf(sesion.getFechaFin()));
+        ps.setInt(4, sesion.getCodTratamiento());
+        ps.setInt(5, sesion.getCodMasajista());
+        ps.setInt(6, sesion.getCodPack());
+        ps.setBoolean(7, sesion.isEstadoInstalacion());
+         int registro = ps.executeUpdate();
+            System.out.println();
+            if (registro > 0) {
+                System.out.println("sesion registrada correctamente" + registro);
+                return ;
+            }
+                
+            }catch(SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error " + ex.getMessage());
+           
+}}
+  public Sesion consultarSesion(int codSesion){
+  Sesion sesion= null;
+        String sql = "SELECT * FROM sesion WHERE codSesion = ?";
+        try {
+          PreparedStatement ps = con.prepareStatement(sql);
+          ps.setInt(1,codSesion);
+          ResultSet resultado = ps.executeQuery();
+          if(resultado.next()){
+          sesion = new Sesion();
+          sesion.setCodSesion(resultado.getInt("codSesion"));
+          sesion.setFechaInicio(resultado.getDate("fechaInicio").toLocalDate());
+          sesion.setFechaFin(resultado.getDate("fechaFin").toLocalDate());
+          sesion.setCodTratamiento(resultado.getInt("codTratamiento"));
+          sesion.setCodMasajista(resultado.getInt("codMasajista"));
+          sesion.setCodPack(resultado.getInt("codPack"));
+          sesion.setEstadoInstalacion(resultado.getBoolean("estadoInstalacion"));
+          
+          }
+      } catch (Exception e){ 
+            System.out.println("error con este codigo");
+      }
+        return sesion;
+        
+        
+  }
 }
