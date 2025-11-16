33a6f02d5a96759b059e6b550484b39af1f35375
alyy <alybelp7@gmail.com>
2025-11-16 14:35:34 -0300

    actualizo la base

--- GP16_Spa.sql
+++ GP16_Spa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2025 a las 06:24:18
+-- Tiempo de generación: 16-11-2025 a las 18:33:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -70,7 +70,8 @@ CREATE TABLE `diaspa` (
 
 INSERT INTO `diaspa` (`codPack`, `FechayHora`, `preferencias`, `codCli`, `monto`, `estadoDia`) VALUES
 (1, '2025-12-01 13:30:00', 'Spa completo corporal y facial', 7, 45600, 1),
-(2, '2025-11-14 13:30:00', 'Masajes faciales', 5, 52500, 0);
+(2, '2025-11-14 13:30:00', 'Masajes faciales', 5, 52500, 0),
+(4, '2025-11-21 16:00:00', 'Solo sauna y piedras calientes', 6, 34000, 1);
 
 -- --------------------------------------------------------
 
@@ -175,7 +176,8 @@ CREATE TABLE `sesion` (
 INSERT INTO `sesion` (`codSesion`, `fechaInicio`, `fechaFin`, `codTratamiento`, `codMasajista`, `codPack`, `codInstal`, `estadoInstalacion`) VALUES
 (2, '2025-11-13 10:00:00', '2025-11-13 10:20:00', 3, 1, 1, 4, 1),
 (4, '2025-11-13 10:30:00', '2025-11-13 11:00:00', 5, 2, 1, 5, 1),
-(7, '2025-11-14 17:30:00', '2025-11-14 17:45:00', 10, 3, 2, 3, 0);
+(7, '2025-11-14 17:30:00', '2025-11-14 17:45:00', 10, 3, 2, 3, 0),
+(8, '2025-11-13 10:30:00', '2025-11-13 11:00:00', 3, 2, 1, 5, 1);
 
 --
 -- Índices para tablas volcadas
@@ -238,7 +240,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `diaspa`
 --
 ALTER TABLE `diaspa`
-  MODIFY `codPack` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `codPack` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `instalacion`
@@ -262,7 +264,7 @@ ALTER TABLE `masajista`
 -- AUTO_INCREMENT de la tabla `sesion`
 --
 ALTER TABLE `sesion`
-  MODIFY `codSesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `codSesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
